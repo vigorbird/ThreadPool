3da455e8aacf78195cac2beeb1335dffab2564a7
fyy <fengyyml@163.com>
2023-11-12 20:30:45 +0800

    add main

--- ThreadPool.h
+++ ThreadPool.h
@@ -15,8 +15,7 @@ class ThreadPool {
 public:
     ThreadPool(size_t);
     template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) 
-        -> std::future<typename std::result_of<F(Args...)>::type>;
+    auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>;
     ~ThreadPool();
 private:
     // need to keep track of threads so we can join them
@@ -34,37 +33,42 @@ private:
 inline ThreadPool::ThreadPool(size_t threads)
     :   stop(false)
 {
+
+    //作者这里使用了lamba表达式来直接构造线程启动需要完成的函数
     for(size_t i = 0;i<threads;++i)
         workers.emplace_back(
+
             [this]
             {
+                //这个就是启动的线程需要完成的工作
                 for(;;)
                 {
                     std::function<void()> task;
 
                     {
                         std::unique_lock<std::mutex> lock(this->queue_mutex);
+                        //如果task不为空则 会执行下面的任务，不会阻塞。如果tasks为空则会阻塞
                         this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
+                                             [this]{ return this->stop || !this->tasks.empty(); });
                         if(this->stop && this->tasks.empty())
                             return;
-                        task = std::move(this->tasks.front());
+                        task = std::move(this->tasks.front());//只有所有权转移 才能执行后面的task任务。
                         this->tasks.pop();
                     }
-
                     task();
                 }
             }
+
         );
 }
 
 // add new work item to the pool
 template<class F, class... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args) 
-    -> std::future<typename std::result_of<F(Args...)>::type>
+auto ThreadPool::enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>
 {
-    using return_type = typename std::result_of<F(Args...)>::type;
+    using return_type = typename std::result_of<F(Args...)>::type;//推断出任务函数的返回值
 
+    //std::packaged_task 包装一个可调用的对象，并且允许异步获取该可调用对象产生的结果
     auto task = std::make_shared< std::packaged_task<return_type()> >(
             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
         );
@@ -77,7 +81,7 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
         if(stop)
             throw std::runtime_error("enqueue on stopped ThreadPool");
 
-        tasks.emplace([task](){ (*task)(); });
+        tasks.emplace(  [task](){(*task)();}  );
     }
     condition.notify_one();
     return res;
